5a356ed190c0f773275175f8d2d68fcf66aebc1f
josh <postor@gmail.com>
2019-04-23 18:43:41 +0800

    config.json

--- app.js
+++ app.js
@@ -2,26 +2,30 @@ const { join } = require('path')
 const { writeFile, ensureDir, remove } = require('fs-extra')
 const exec = require('await-exec')
 const express = require('express')
+require('express-async-errors')
 const { json } = require('body-parser')
 const uniqid = require('uniqid')
+const camelCase = require('camelcase')
+const { languages } = require('./config')
 const app = express()
 const port = 3002
 
 console.log('Make sure you run on Linux, got Docker and Node image!')
-const nodeImage = process.env.RUNNER_NODE_IMAGE || 'postor/leetcode-mvp-runner'
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use(json())
 app.post('/', async (req, res) => {
-  const { key, code } = req.body
+  const { key, code, language = 'nodejs' } = req.body
   const fileName = uniqid() + '.js'
   const tmpDir = process.env.RUNNER_TEMP_DIR || join(__dirname, '.tmp')
   await ensureDir(tmpDir)
   const filePath = join(tmpDir, fileName)
   await writeFile(filePath, code)
   const dockerMapPath = `/app/answers/${key}.js`
-  const jestCmd = `yarn jest ${key}.test.js`
   const vfile = `-v ${filePath}:${dockerMapPath}`
-  const command = `docker run --rm ${vfile} ${nodeImage} ${jestCmd}`
+  const env = `-e "TESTKEY=${key}" -e "TESTKEYCAMEL=${camelCase(key, { pascalCase: true })}"`
+  const { image } = languages[language]
+  const command = `docker run --rm ${vfile} ${env} ${image}`
   try {
     let rtn = await exec(command)
     await remove(filePath)
@@ -33,8 +37,10 @@ app.post('/', async (req, res) => {
 });
 
 (async () => {
-  console.log(`pulling docker image ${nodeImage}`)
-  await exec(`docker pull ${nodeImage}`)
-  console.log(`docker image ${nodeImage} loaded!`)
+  Object.keys(languages).forEach(({ image }) => {
+    console.log(`pulling docker image ${image}`)
+    await exec(`docker pull ${image}`)
+    console.log(`docker image ${image} loaded!`)
+  })
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 })()
